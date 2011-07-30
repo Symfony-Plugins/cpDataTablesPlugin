47e9b920bb2da8f7ea929470ce14626bf519e619
coopers <coopers@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-07-30 08:01:38 -0500

    added us date and formatted n umber sorting methods
    git-svn-id: http://svn.symfony-project.com/plugins/cpDataTablesPlugin/trunk@32848 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/js/jquery.dataTables.ext.js
+++ web/js/jquery.dataTables.ext.js
@@ -5,10 +5,14 @@
     if (date.indexOf('.') > 0) {
       /*date a, format dd.mn.(yyyy) ; (year is optional)*/
       var eu_date = date.split('.');
-    } 
+    }
     else if (date.indexOf('/') > 0){
       /*date a, format dd/mn/(yyyy) ; (year is optional)*/
       var eu_date = date.split('/');
+    }
+    else if (date.indexOf('-') > 0){
+      /*date a, format dd-mn-(yyyy) ; (year is optional)*/
+      var eu_date = date.split('-');
     }
     else { return 0; }
     
@@ -46,3 +50,70 @@
     return ((x < y) ? 1 : ((x > y) ?  -1 : 0));
   };  
 })(jQuery);
+
+(function($) {
+  function calculate_date(date) {
+    var date = date.replace(" ", "");
+    
+    if (date.indexOf('/') > 0){
+      /*date a, format mn/dd/(yyyy) ; (year is optional)*/
+      var eu_date = date.split('/');
+    }
+    else if (date.indexOf('-') > 0){
+      /*date a, format mn-dd-(yyyy) ; (year is optional)*/
+      var eu_date = date.split('-');
+    }
+    else { return 0; }
+    
+    
+    /*year (optional)*/
+    if (eu_date[2]) { var year = eu_date[2]; } 
+    else { var year = 0; }
+    
+    /*month*/
+    var monthday = eu_date[0];
+    if (month.length == 1) {
+      month = 0+month;
+    }
+    
+    /*day*/
+    var day = eu_date[1];
+    if (day.length == 1) {
+      day = 0+day;
+    }
+    
+    return (year + month + day) * 1;
+  }
+
+  $.fn.dataTableExt.oSort['us_date-asc'] = function(a, b) {
+    x = calculate_date(a);
+    y = calculate_date(b);
+    
+    return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+  };
+
+  $.fn.dataTableExt.oSort['us_date-desc'] = function(a, b) {
+    x = calculate_date(a);
+    y = calculate_date(b);
+    
+    return ((x < y) ? 1 : ((x > y) ?  -1 : 0));
+  };  
+})(jQuery);
+
+(function($) {
+  jQuery.fn.dataTableExt.oSort['formatted-num-asc'] = function(x,y){
+    x = x.replace(/[^\d\-\.\/]/g,'');
+    y = y.replace(/[^\d\-\.\/]/g,'');
+    if(x.indexOf('/')>=0)x = eval(x);
+    if(y.indexOf('/')>=0)y = eval(y);
+    return x/1 - y/1;
+  };
+  jQuery.fn.dataTableExt.oSort['formatted-num-desc'] = function(x,y){
+    x = x.replace(/[^\d\-\.\/]/g,'');
+    y = y.replace(/[^\d\-\.\/]/g,'');
+    if(x.indexOf('/')>=0)x = eval(x);
+    if(y.indexOf('/')>=0)y = eval(y);
+    return y/1 - x/1;
+  };
+})(jQuery);
+}
